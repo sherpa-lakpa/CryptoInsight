4296c90e411392ec5fbce6a60186d56bd5559e5f
sherpa-lakpa <sherpalakpa18@gmail.com>
2026-03-28 17:31:43 +0000

    Framework updates for pipeline

--- utils/api_client.py
+++ utils/api_client.py
@@ -1,6 +1,35 @@
 import requests
 
+
 def fetch_crypto_data(coins):
+    all_data = []
+    for coin in coins:
+        url = f"https://api.coingecko.com/api/v3/coins/{coin}/market_chart?vs_currency=usd&days=1"
+        
+        for attempt in range(3):
+            response = requests.get(url)
+            if response.status_code == 200:
+                break
+            time.sleep(2)
+        
+        if response.status_code == 200:
+            data = response.json()
+            
+            prices = data["prices"]  # [timestamp, price]
+            
+            for record in prices:
+                all_data.append({
+                    "coin": coin,
+                    "timestamp": record[0],
+                    "price": record[1]
+                })
+        else:
+            print(f"Failed for {coin}")
+
+    return all_data
+
+
+def fetch_crypto_current_data(coins):
     url = "https://api.coingecko.com/api/v3/simple/price"
 
     params = {
